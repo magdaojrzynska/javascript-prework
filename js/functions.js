644035ed584a7c0951ddd8284c8112f4509a68d7
Magdalena Ojrzynska <magdalena.ojrzynska@gmail.com>
2024-04-04 19:08:07 +0200

    Add elfe if function

--- js/functions.js
+++ js/functions.js
@@ -8,6 +8,14 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2' ) {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+    computerMove = 'nozyce'
+}
+printMessage('Mój ruch: ' + computerMove);
